0b6d26ff4efd9ebd073f2e264acb996c70d3c9b6
Tomasz Fiechowski <tomasz.fiechowski@codility.com>
2024-02-03 17:54:38 +0100

    Add sidebar

---
+++ app/app/layout.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { Sidebar } from '@/components/ui/Sidebar';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="bg-background min-h-screen">
+      <div className="grid lg:grid-cols-5 min-h-screen">
+        <Sidebar />
+        <div className="col-span-3 lg:col-span-4 lg:border-l">
+          <div className="h-full px-4 py-6 lg:px-8">{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/ui/Sidebar.tsx
@@ -0,0 +1,133 @@
+import { cn } from '@/lib/utils';
+import { Button } from '@/registry/new-york/ui/button';
+import { ScrollArea } from '@/registry/new-york/ui/scroll-area';
+
+export type Playlist = (typeof playlists)[number];
+
+export const playlists = [
+  'Recently Added',
+  'Recently Played',
+  'Top Songs',
+  'Top Albums',
+  'Top Artists',
+  'Logic Discography',
+  'Bedtime Beats',
+  'Feeling Happy',
+  'I miss Y2K Pop',
+  'Runtober',
+  'Mellow Days',
+  'Eminem Essentials',
+];
+
+interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function Sidebar({ className }: SidebarProps) {
+  return (
+    <div className={cn('flex', className)}>
+      <div className=" grow flex flex-col space-y-4 py-4">
+        <div className="px-3 py-2">
+          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">Nuggets</h2>
+          <div className="space-y-1">
+            <Button variant="ghost" className="w-full justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-4 w-4"
+              >
+                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+                <circle cx="12" cy="7" r="4" />
+              </svg>
+              Competetive notes
+            </Button>
+
+            <Button variant="ghost" className="w-full justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-4 w-4"
+              >
+                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+                <circle cx="12" cy="7" r="4" />
+              </svg>
+              Calls
+            </Button>
+
+            {/* See more example items here at https://github.com/shadcn-ui/ui/blob/f859d4857eab19989cb01582775f21d6ef91c5b4/apps/www/app/examples/music/components/sidebar.tsx */}
+          </div>
+        </div>
+        <div className="px-3 py-2">
+          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">Team</h2>
+          <div className="space-y-1">
+            <Button variant="ghost" className="w-full justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-4 w-4"
+              >
+                <path d="M21 15V6" />
+                <path d="M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
+                <path d="M12 12H3" />
+                <path d="M16 6H3" />
+                <path d="M12 18H3" />
+              </svg>
+              Manage
+            </Button>
+            <Button variant="ghost" className="w-full justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-4 w-4"
+              >
+                <circle cx="8" cy="18" r="4" />
+                <path d="M12 18V2l7 4" />
+              </svg>
+              Members
+            </Button>
+          </div>
+        </div>
+        <div className="grow px-3 py-2" />
+        <div className="px-3 py-2">
+          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">Account</h2>
+          <div className="space-y-1">
+            <Button variant="ghost" className="w-full justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-4 w-4"
+              >
+                <circle cx="8" cy="18" r="4" />
+                <path d="M12 18V2l7 4" />
+              </svg>
+              Logout
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
