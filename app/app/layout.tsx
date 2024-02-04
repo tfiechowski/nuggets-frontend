145eec6a8055f4758906c68b2bf2896257b82f95
Tomasz Fiechowski <tomasz.fiechowski@codility.com>
2024-02-04 18:05:13 +0100

    Add Toaster

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { Sidebar } from '@/components/ui/Sidebar';
+import { Toaster } from '@/components/ui/sonner';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-background min-h-screen">
+      <Toaster />
       <div className="grid lg:grid-cols-5 min-h-screen">
         <Sidebar />
         <div className="col-span-3 lg:col-span-4 lg:border-l">
