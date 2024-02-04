bdf9e7a0e68bae954192211a46e56876dfde85bd
Tomasz Fiechowski <tomasz.fiechowski@codility.com>
2024-02-04 12:43:25 +0100

    Add avatar

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -1,7 +1,9 @@
 import { cn } from '@/lib/utils';
 import { Button } from '@/registry/new-york/ui/button';
-import { ScrollArea } from '@/registry/new-york/ui/scroll-area';
+import NuggetsLogo from '@/app/public/images/nuggets-logo.png';
 import Link from 'next/link';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import Image from 'next/image';
 
 export type Playlist = (typeof playlists)[number];
 
@@ -22,12 +24,28 @@ export const playlists = [
 
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+function Header() {
+  return (
+    <div className="flex flex-row py-2 items-center">
+      <Avatar className="w-8 h-8">
+        <AvatarImage asChild src={NuggetsLogo.src}>
+          <Image src={NuggetsLogo.src} width={32} height={32} alt="XD" />
+        </AvatarImage>
+        <AvatarFallback>N</AvatarFallback>
+      </Avatar>
+
+      <h2 className="px-4 text-lg font-semibold tracking-tight">Nuggets</h2>
+    </div>
+  );
+}
+
 export function Sidebar({ className }: SidebarProps) {
   return (
-    <div className={cn('flex', className)}>
-      <div className=" grow flex flex-col space-y-4 py-4">
+    <div className={cn('pb-12 flex', className)}>
+      <div className="grow flex flex-col space-y-4 py-4">
         <div className="px-3 py-2">
-          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">Nuggets</h2>
+          <Header />
+
           <div className="space-y-1">
             <Link href="/app/core/competitors">
               <Button variant="ghost" className="w-full justify-start">
