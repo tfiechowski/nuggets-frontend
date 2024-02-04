c78d1d46f095bf06da211bf5fc1a96a18a391436
Tomasz Fiechowski <tomasz.fiechowski@codility.com>
2024-02-04 13:44:19 +0100

    List team members

--- app/app/team/members/page.tsx
+++ app/app/team/members/page.tsx
@@ -1,6 +1,29 @@
-'use server';
-
+import { DataTable } from '@/app/app/team/members/MembersTable';
 import { getServerSupabaseClient } from '@/app/utils/server/getServerSupabaseClient';
+import { ColumnDef } from '@tanstack/react-table';
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type User = {
+  name: string;
+  email: string;
+  role: string;
+};
+
+export const columns: ColumnDef<User>[] = [
+  {
+    accessorKey: 'name',
+    header: 'Name',
+  },
+  {
+    accessorKey: 'email',
+    header: 'Email',
+  },
+  {
+    accessorKey: 'role',
+    header: 'Role',
+  },
+];
 
 async function getUserTeam(): Promise<{ accountId: string }> {
   const supabase = getServerSupabaseClient();
@@ -22,23 +45,35 @@ async function getUserTeam(): Promise<{ accountId: string }> {
   return { accountId: team.account_id };
 }
 
-async function getMembers() {
+async function getMembers(): Promise<User[]> {
   const supabase = getServerSupabaseClient();
 
   const userTeamAccount = await getUserTeam();
 
-  return supabase.rpc('get_account_members', {
+  const { data, error } = await supabase.rpc('get_account_members', {
     account_id: userTeamAccount.accountId,
   });
+
+  if (error) {
+    console.log(error);
+    throw new Error(error.message);
+  }
+
+  return data.map((member: any) => ({
+    name: member.name,
+    email: member.email,
+    role: member.account_role,
+  }));
 }
 
 export default async function Manage() {
   const members = await getMembers();
+  console.log('🚀 ~ Manage ~ members:', members);
 
   return (
     <div>
-      Members!
-      <div>{JSON.stringify(members)}</div>
+      <DataTable columns={columns} data={members} />
+      {/* <div>{JSON.stringify(members)}</div> */}
     </div>
   );
 }
