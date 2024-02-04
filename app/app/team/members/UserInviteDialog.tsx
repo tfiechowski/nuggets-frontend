57e116e0f67e4c9f84543b21be95fe1a72da1591
Tomasz Fiechowski <tomasz.fiechowski@codility.com>
2024-02-04 18:31:53 +0100

    Add comment

--- app/app/team/members/UserInviteDialog.tsx
+++ app/app/team/members/UserInviteDialog.tsx
@@ -27,6 +27,14 @@ export function UserInviteDialog({}: {}) {
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <UserInviteForm
+          // TODO: extract to true dependency injection or API context in the future
+          // Maybe even use the API Context in the form itself?
+          // Like, this form is very specifc and will not be reused
+          // so maybe there is no point in having a specific Dialog here as well
+          // which is just passing a proper API method below
+          // However, eg. this Dialog could be a generic one,
+          // given form eg (Form and FormInner components) gets
+          // its api inside
           onInvite={TeamInvitationService.inviteUserToTeam}
           onSuccess={handleSuccess}
         />
