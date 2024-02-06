e002dfd2e7997811feeb205aafea45137c289e38
Tomasz Fiechowski <tomasz.fiechowski@codility.com>
2024-02-06 23:20:34 +0100

    Add error handling

--- app/utils/server/getTeamInvitations.ts
+++ app/utils/server/getTeamInvitations.ts
@@ -3,17 +3,26 @@ import { getUserTeam } from '@/app/utils/server/getUserTeam';
 
 export async function getTeamInvitations(): Promise<number> {
   const supabase = getServerSupabaseClient();
+  const user = await supabase.auth.getUser();
 
   const userTeamAccount = await getUserTeam();
 
-  const { data, error } = await supabase.rpc('get_account_invitations', {
+  const response = await supabase.rpc('get_account_invitations', {
     account_id: userTeamAccount.accountId,
   });
+  const { data, error } = response;
 
   if (error) {
     console.log(error);
     throw new Error(error.message);
   }
 
-  return data.length;
+  if (data) {
+    return data.length;
+  }
+
+  console.error(
+    `Error listing invitations for account: ${userTeamAccount.accountId} by ${user.data.user?.email}. Response data was null, not an array as expected.`
+  );
+  return 0;
 }
