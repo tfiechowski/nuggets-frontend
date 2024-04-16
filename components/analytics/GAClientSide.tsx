4d5d5d87e53cb91120155c3e6a924c9124f98f8a
Tomasz Fiechowski <tomasz.fiechowski@codility.com>
2024-04-16 15:56:31 +0200

    Fix init condition for GA

--- components/analytics/GAClientSide.tsx
+++ components/analytics/GAClientSide.tsx
@@ -6,7 +6,7 @@ import ReactGA from 'react-ga4';
 
 export default function GACLientSide({ userId }: { userId: string }) {
   useEffect(() => {
-    if (NODE_ENV !== 'production') {
+    if (NODE_ENV === 'production') {
       console.log(`Initialising ReactGA for user ${userId}`);
 
       ReactGA.initialize([
@@ -17,8 +17,6 @@ export default function GACLientSide({ userId }: { userId: string }) {
       ]);
     }
   }, []);
-
-  console.log('ASD');
 
   return <></>;
 }
