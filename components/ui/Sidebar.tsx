95fc5274b2941e7b3116f00cc32989791d8652e4
Tomasz Fiechowski <tomasz.fiechowski@codility.com>
2024-05-22 20:29:47 +0200

    Cleanup

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -7,23 +7,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Calendar } from 'lucide-react';
 
-export type Playlist = (typeof playlists)[number];
-
-export const playlists = [
-  'Recently Added',
-  'Recently Played',
-  'Top Songs',
-  'Top Albums',
-  'Top Artists',
-  'Logic Discography',
-  'Bedtime Beats',
-  'Feeling Happy',
-  'I miss Y2K Pop',
-  'Runtober',
-  'Mellow Days',
-  'Eminem Essentials',
-];
-
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 function Header() {
