897d76a2453a9654e5346142a2152d9afde17ed6
Tomasz Fiechowski <tomasz.fiechowski@codility.com>
2024-05-22 20:50:12 +0200

    Remove unused code

--- app/context/utils/useCurrentTeamMembers.ts
+++
@@ -1,6 +0,0 @@
-import { UserContext } from '@/app/context/UserContext';
-import { useContext } from 'react';
-
-export function useCurrentTeamMembers() {
-  const { account } = useContext(UserContext);
-}
