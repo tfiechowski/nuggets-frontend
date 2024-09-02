52eec400e520447db20e94ae86f81a8b0a0a3e40
Tomasz Fiechowski <tomasz.fiechowski@codility.com>
2024-09-02 20:05:29 +0200

    Fix privacy policy

--- components/landing/Footer.tsx
+++ components/landing/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
   ];
   const legal = [
     { title: 'Terms', link: '/legal/terms-of-service' },
-    { title: 'Privacy', link: '/legal/privacy-policy' },
+    { title: 'Privacy Policy', link: '/legal/privacy-policy' },
     { title: 'Support', link: '/support' },
   ];
   return (
